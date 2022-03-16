0646b183f37ccee9ce1cf6834996bb3bf47338e6
Marquis Sampson <marquis4484@gmail.com>
2022-03-16 18:11:23 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
+
+
+function App() {
+
+  const [data,setData] = useState({}) //
+  const[location,setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=8369e6d431e66dc0a9a920872a693b5c`
+  
+  useEffect(() => {
+    document.title = "Weather App";  
+  }, []);
+ 
+  const searchLocation = (e) => {
+    if(e.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  return (
+    <div className="App">
+      <nav>Weather App</nav>
+      <div className="search">
+         <input
+        value={location}
+        onChange={e => setLocation(e.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Please enter your location'
+        type="text"/>
+      </div>
+      <div className="container">
+       <div className="upper">
+        
+          <div className='location'>
+            <p>{data.name}</p>
+          </div>
+           <div className='temp'>
+             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+           </div>
+           <div className='description'>
+             {data.weather ? <p>Condition: {data.weather[0].main}</p> : null}
+             
+        </div>
+      </div>
+          {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            
+          </div>
+}
+      </div>
+    </div>
+  );
+}
+
+export default App;
